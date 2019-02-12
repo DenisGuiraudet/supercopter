865c459a3b005d08e689aac1be757db10f38defc
AlexandrePause <pausealexandre@gmail.com>
2019-02-12 22:46:51 +0100

    Implement frontend and backend with basic data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,35 @@ import { RouterModule, Routes } from '@angular/router';
  
 import { PageLoginComponent }   from './pages/page-login/page-login.component';
 import { PageListingComponent } from './pages/page-listing/page-listing.component';
+import { GstAddComponent } from './gst-add/gst-add.component';
+import { GstEditComponent } from './gst-edit/gst-edit.component';
+import { GstGetComponent } from './gst-get/gst-get.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: PageLoginComponent },
-  { path: 'listing', component: PageListingComponent }
+  { 
+    path: '', redirectTo: '/business', pathMatch: 'full' 
+  },
+  { 
+    path: 'login', 
+    component: PageLoginComponent 
+  },
+  { 
+    path: 'listing', 
+    component: PageListingComponent 
+  },
+  {
+    path: 'business/create',
+    component: GstAddComponent
+  },
+  {
+    path: 'business/edit/:id',
+    component: GstEditComponent
+  },
+  {
+    path: 'business',
+    component: GstGetComponent
+  }
+
 ];
  
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
 import { PageListingComponent } from './pages/page-listing/page-listing.component';
+import { GstAddComponent } from './gst-add/gst-add.component';
+import { GstGetComponent } from './gst-get/gst-get.component';
+import { GstEditComponent } from './gst-edit/gst-edit.component';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BusinessService } from './business.service';
+
+
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   declarations: [
     AppComponent,
     PageLoginComponent,
-    PageListingComponent
+    PageListingComponent,
+    GstAddComponent,
+    GstGetComponent,
+    GstEditComponent
   ],
-  providers: [],
+  providers: [ BusinessService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
